55ce1e8ba7d402ac7eb8f4a7741424bf0b5107aa
Beardedrasta <40687057+Beardedrasta@users.noreply.github.com>
2023-06-05 22:23:19 -0400

    Added: Reactive Run bar, Reactive special attack bar, text/icon options, position fix for when bars have been disbaled, (also fixed positionioning from previous import that made the bars seperate from the numbers. :))

--- src/main/java/com/RastaXP/RastaXPConfig.java
+++ src/main/java/com/RastaXP/RastaXPConfig.java
@@ -6,34 +6,78 @@ import net.runelite.client.config.Alpha;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 import java.awt.*;
 
 @ConfigGroup("RastaXP")
 public interface RastaXPConfig extends Config
 {
-	Color EXP_COLOR = new Color(130, 60, 170, 175);
+	Color EXP_COLOR = new Color(130, 60, 170, 195);
+
+	@ConfigSection(
+			name = "Toggles",
+			description = "On / Off toggle options.",
+			position = 97
+	)
+	String toggleSection = "toggles";
+
+	@ConfigSection(
+			name = "Skilling",
+			description = "Expereince / Skill related options.",
+			position = 98
+	)
+	String xpSection = "skilling";
+
+	@ConfigSection(
+			name = "Misc",
+			description = "List of config options.",
+			position = 99
+	)
+	String miscSection = "misc";
+
 	@ConfigItem(
-			position = 0,
+			position = 1,
 			keyName = "XPtracker",
 			name = "Experience Bar",
-			description = "Displays the experience bar."
+			description = "Displays the experience bar.",
+			section = toggleSection
 	)
 	default boolean XPtracker() { return true; }
 
 	@ConfigItem(
-			position = 1,
+			position = 2,
 			keyName = "displayHealthAndPrayer",
 			name = "Status Bars",
-			description = "Displays Healh and Prayer."
+			description = "Displays Healh and Prayer.",
+			section = toggleSection
 	)
-	default boolean displayHealthAndPrayer() { return false; }
+	default boolean displayHealthAndPrayer() { return true; }
 
 	@ConfigItem(
-			position = 2,
+			position = 3,
+			keyName = "displayRun",
+			name = "Run Energy",
+			description = "Displays run energy bar.",
+			section = toggleSection
+	)
+	default boolean displayRun() { return false; }
+
+	@ConfigItem(
+			position = 4,
+			keyName = "displaySpecial",
+			name = "Special Attack",
+			description = "Displays the special bar.",
+			section = toggleSection
+	)
+	default boolean displaySpecial() { return true; }
+
+	@ConfigItem(
+			position = 5,
 			keyName = "skill",
 			name = "Active Skill",
-			description = "Choose which skill to track when Recent Skill setting is disabled."
+			description = "Choose which skill to track when Recent Skill setting is disabled.",
+			section = xpSection
 	)
 	default Skill skill()
 	{
@@ -41,18 +85,20 @@ public interface RastaXPConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 3,
+			position = 6,
 			keyName = "mostRecentSkill",
-			name = "Toggle Recent Skill",
-			description = "Display the most recent skill trained."
+			name = "Recent Skill",
+			description = "Display the most recent skill trained.",
+			section = xpSection
 	)
 	default boolean mostRecentSkill() { return false; }
 
 	@ConfigItem(
-			position = 4,
+			position = 7,
 			keyName = "enableSkillIcon",
 			name = "Icons",
-			description = "Displays prayer and health icons."
+			description = "Displays prayer and health icons.",
+			section = xpSection
 	)
 	default boolean enableSkillIcon()
 	{
@@ -60,10 +106,11 @@ public interface RastaXPConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 5,
+			position = 8,
 			keyName = "enableSkillText",
 			name = "Text",
-			description = "Displays prayer and health status text."
+			description = "Displays prayer and health status text.",
+			section = xpSection
 	)
 	default boolean enableSkillText()
 	{
@@ -71,10 +118,23 @@ public interface RastaXPConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 6,
+			position = 9,
+			keyName = "enableTip",
+			name = "Tooltip",
+			description = "Displays xp tracked info on mouseover.",
+			section = miscSection
+	)
+	default boolean enableTip()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			position = 10,
 			keyName = "enableSmall",
 			name = "Small Bars",
-			description = "Display bars at a reduced size."
+			description = "Display bars at a reduced size.",
+			section = miscSection
 	)
 	default boolean enableSmall()
 	{
@@ -83,22 +143,11 @@ public interface RastaXPConfig extends Config
 
 	@Alpha
 	@ConfigItem(
-			position = 7,
-			keyName = "hpbarColor",
-			name = "Health Color",
-			description = "Configures the color of the Health bar"
-	)
-	default Color colorHP()
-	{
-		return Color.RED;
-	}
-
-	@Alpha
-	@ConfigItem(
-			position = 8,
+			position = 11,
 			keyName = "xpbarColor",
 			name = "Experience Color",
-			description = "Configures the color of the Experience bar"
+			description = "Configures the color of the Experience bar",
+			section = miscSection
 	)
 	default Color colorXP()
 	{
@@ -107,14 +156,15 @@ public interface RastaXPConfig extends Config
 
 	@Alpha
 	@ConfigItem(
-			position = 9,
+			position = 12,
 			keyName = "xpbarNotchColor",
 			name = "Segment Color",
-			description = "Configures the color of the experience segments."
+			description = "Configures the color of the experience segments.",
+			section = miscSection
 	)
 	default Color colorXPNotches()
 	{
-		return Color.LIGHT_GRAY;
+		return new Color(909090);
 	}
 
 
